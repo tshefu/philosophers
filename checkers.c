993598f472c28b1c4c8ec74cc65f445b97eed6b0
Viktoria Schneider <vschneid@c3r7p3.42vienna.com>
2023-12-04 17:42:35 +0100

    modified input check, philos not over 200

--- checkers.c
+++ checkers.c
@@ -6,34 +6,39 @@
 /*   By: vschneid <vschneid@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 14:51:18 by vschneid          #+#    #+#             */
-/*   Updated: 2023/12/01 18:47:21 by vschneid         ###   ########.fr       */
+/*   Updated: 2023/12/04 17:15:29 by vschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void    input_check(int argc, char **argv)
+void input_check(int argc, char **argv)
 {
-    char    input[MAXINPUT] = "";
-    int     length;
-    int     i;
-    int     j;
+  char input[MAXINPUT];
+  int length;
+  int i;
+  int j;
+  int philos;
 
-    j = 1;
-    while (j < argc)
-    {
-        ft_strcpy(input, argv[j]);
-        length = ft_strlen(input);
-        if (input[0] == '0')
-            input_error(NULL);
-        i = 0;
-        while (i < length)
-        {
-            if (!ft_isdigit(input[i]))
-                input_error(NULL);
-            i++;
-        }
-        j++;
-    }
+  j = 1;
+  while (j < argc)
+  {
+      ft_strcpy(input, argv[j]);
+      length = ft_strlen(input);
+      philos = ft_atoi(argv[1]);
+      if (philos > 200)
+          input_error(NULL);
+      if (input[0] == '0')
+          input_error(NULL);
+      i = 0;
+      while (i < length)
+      {
+          if (!ft_isdigit(input[i]))
+              input_error(NULL);
+          i++;
+      }
+      j++;
+  }
 }
+
 
